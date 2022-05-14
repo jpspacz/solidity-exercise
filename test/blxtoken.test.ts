3866dfdfa0db6374e575186af0c6595d2de39b25
jonspa <jpspaczynski@gmail.com>
2022-05-14 12:11:52 +0200

    test initial states for contract

--- test/blxtoken.test.ts
+++ test/blxtoken.test.ts
@@ -1,14 +1,21 @@
 import { ethers } from "hardhat";
-import { expect } from "chai";
+const assert = require("assert");
 
 describe("BLX Token contract", function () {
-  it("default test", async function () {
-    // const [owner] = await ethers.getSigners();
+
+  it("initial state should have name as Bloxify Token, symbol as BLX, decimals as 18 and totalSupply as 0", async function () {
+    const name = "Bloxify Token";
+    const symbol = "BLX";
+    const initialSupply = 0;
+    const decimals = 18;
+
     const factory = await ethers.getContractFactory("BLXToken");
-    const contract = await factory.deploy();
+    const contract = await factory.deploy(name, symbol);
 
-    const greeting = await contract.greeting();
-
-    expect(greeting).to.equal("Hello you");
+    assert.equal(await contract.totalSupply(), initialSupply);
+    assert.equal(await contract.name(), name);
+    assert.equal(await contract.symbol(), symbol);
+    assert.equal(await contract.decimals(), decimals);
   });
+
 });
