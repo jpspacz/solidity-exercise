6dcb6824d5e663cb106d5bcb83b40b952f84f310
jonspa <jpspaczynski@gmail.com>
2022-05-14 12:32:02 +0200

    check if token balance changes correctly

--- test/blxtoken.test.ts
+++ test/blxtoken.test.ts
@@ -18,4 +18,31 @@ describe("BLX Token contract", function () {
     assert.equal(await contract.decimals(), decimals);
   });
 
+  it("balance of token should change accordingly to mint and transfer methods", async function () {
+    const [addr1, addr2] = await ethers.getSigners();
+
+    const factory = await ethers.getContractFactory("BLXToken");
+    const contract = await factory.deploy("Bloxify Token", "BLX");
+    const tokenAmount = 100;
+
+    // initial balance should be 0
+    assert.equal(await contract.balanceOf(addr1.address), 0);
+    
+    // raise initial balance by minting new coins
+    contract.mint(tokenAmount);
+
+    // check if balance changed
+    assert.equal(await contract.balanceOf(addr1.address), tokenAmount);
+
+    // send coins to decrease balance
+    contract.transfer(addr2.address, tokenAmount);
+
+    // balance should revert to 0
+    assert.equal(await contract.balanceOf(addr1.address), 0);
+
+    // balance of addr2 should increse by sent amount
+    assert.equal(await contract.balanceOf(addr2.address), tokenAmount);
+
+  });
+
 });
